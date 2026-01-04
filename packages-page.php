83edae017baced7838802c78bec102518e7a4f7f
closed-gates <ibsanhosainansary@gmail.com>
2026-01-04 14:51:46 +0600

    created a general nav php file which i included in every file
    i was annoyed by having to change the nav multiple times so i made a separate file to include in every page.

--- packages-page.php
+++ packages-page.php
@@ -18,16 +18,7 @@
 </head>
 <body>
     <div class="container">
-        <nav class="sidebar">
-            <div class="logo"><a href = "Home-page.php">ResiCare</a></div>
-            <ul>
-                <li class="active"><a href="#">Dashboard</a></li>
-                <li><a href="Residents-page.php">Residents</a></li>
-                <li><a href="Service-page.php">Maintenance</a></li>
-                <li><a href="payment-history.php">Payments</a></li>
-                <li><a href="review-page.php">Review</a></li>
-            </ul>
-        </nav>
+        <?php include 'head-nav.php'; ?>
 
         <main class="main-content">
             <section class="table-section">

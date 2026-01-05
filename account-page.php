8642f7bf82ee095640e313dac846061df2028912
closed-gates <ibsanhosainansary@gmail.com>
2026-01-05 19:18:46 +0600

    small bug fixes

--- account-page.php
+++ account-page.php
@@ -1,71 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php 
-     require_once("auth.php");
-     require_once("Dbconnect.php");
-     $sql  = "select * from user_info";
-     $row = mysqli_fetch_array(mysqli_query($conn,$sql));
-    ?>
+<?php
+require_once("auth.php");
+require_once("Dbconnect.php");
+$sql = "select * from user_info where nid = " . $_SESSION['user_id'] . "";
+$row = mysqli_fetch_array(mysqli_query($conn, $sql));
+?>
 <?php include 'head.php'; ?>
+
 <body>
     <div class="container">
         <?php include 'head-nav.php'; ?>
-    <main class="main-content">
-        <div style="display:flex; justify-content:flex-end;">
-            <form action="change-page.php" class="form_design" method="POST">
+        <main class="main-content">
+            <div style="display:flex; justify-content:flex-end;">
+                <form action="change-page.php" class="form_design" method="POST">
                     <button type="submit" class="btn-primary">Change Account Details</button>
-            </form>
-        </div>
+                </form>
+            </div>
             <div class="card">
                 <h3>NID</h3>
                 <p class="stat-number">
-                    <?php echo $_SESSION['user_id'];?>
+                    <?php echo $_SESSION['user_id']; ?>
                 </p>
             </div>
             <div class="card">
                 <h3>UserName</h3>
-                <p class="stat-number" >
-                    <?php echo $_SESSION['username']?>
+                <p class="stat-number">
+                    <?php echo $_SESSION['username'] ?>
                 </p>
             </div>
             <div class="card">
                 <h3>Date of Birth</h3>
-                <p class="stat-number" >
-                    <?php echo $row[2]?>
+                <p class="stat-number">
+                    <?php echo $row[2] ?>
                 </p>
             </div>
             <div class="card">
                 <h3>Street</h3>
-                <p class="stat-number" >
+                <p class="stat-number">
                     <?php echo $row[3];
-                ?>
+                    ?>
                 </p>
             </div>
             <div class="card">
                 <h3>City</h3>
-                <p class="stat-number" >
-                    <?php echo $row[4];?>
+                <p class="stat-number">
+                    <?php echo $row[4]; ?>
                 </p>
             </div>
             <div class="card">
                 <h3>District</h3>
-                <p class="stat-number" >
-                    <?php echo $row[5];?>
+                <p class="stat-number">
+                    <?php echo $row[5]; ?>
                 </p>
             </div>
             <div class="card">
                 <h3>Has A Membership</h3>
-                <p class="stat-number" >
-                    <?php echo $row[6];?>
+                <p class="stat-number">
+                    <?php echo $row[6]; ?>
                 </p>
             </div>
             <div class="card">
                 <h3>Membership Referer</h3>
-                <p class="stat-number" >
-                    <?php echo $row[7];?>
+                <p class="stat-number">
+                    <?php echo $row[7]; ?>
                 </p>
             </div>
 
-    </main>
+        </main>
 </body>
+
 </html>
--- review-page.php
+++ review-page.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php 
-     require_once('auth.php')
+<?php
+require_once('auth.php')
     ?>
 <?php include 'head.php'; ?>
+
 <body>
     <div class="container">
         <?php include 'head-nav.php'; ?>
 
-    <main class="main-content">
-        <form action ="review.php" class="form_design" method="POST"> 
-                    Which Flat You Want To Review: <input type="number" name="pin" placeholder="pin" required> </br>
-                    Suggetion: <input type="text" name="sugg"> </br>
-                    Complaint: <input type="text" name="com"> </br>
-                    Feedback: <input type="text" name="feed"> </br>
-                    <br><br><br>
-                    <input type="hidden" name="nid" value="<?php echo $_SESSION['user_id']?>">
-                    <input type="hidden" name="token" value="<?php echo random_int(100000, 999999)?>">
-                    <button type="submit" class="btn-primary">Review</button>
+        <main class="main-content">
+            <form action="review.php" class="form_design" method="POST">
+                Which Flat You Want To Review: <input type="number" name="pin" placeholder="pin" required> </br>
+                Suggestion: <input type="text" name="sugg"> </br>
+                Complaint: <input type="text" name="com"> </br>
+                Feedback: <input type="text" name="feed"> </br>
+                <br><br><br>
+                <input type="hidden" name="nid" value="<?php echo $_SESSION['user_id'] ?>">
+                <input type="hidden" name="token" value="<?php echo random_int(100000, 999999) ?>">
+                <button type="submit" class="btn-primary">Review</button>
             </form>
-    </main>
+        </main>
 </body>
+
 </html>

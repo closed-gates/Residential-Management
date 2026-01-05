bdeac3abe3c11139339565debd2e95035ad482eb
closed-gates <ibsanhosainansary@gmail.com>
2026-01-05 20:06:05 +0600

    username instead of nid

--- Residents-page.php
+++ Residents-page.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php 
-     require_once('auth.php')
+<?php
+require_once('auth.php')
     ?>
 <?php include 'head.php'; ?>
+
 <body>
     <div class="container">
         <?php include 'head-nav.php'; ?>
@@ -14,41 +15,43 @@
                 <table id="requestTable">
                     <thead>
                         <tr>
-                            <th>NID</th>
+                            <th>Name</th>
                             <th>Occupation</th>
                             <th>Student</th>
                             <th>Currently Renting</th>
                             <th>Rental History</th>
                         </tr>
-                        <?php 
-                            require_once('DBconnect.php');
-                            $sql = "select * from renters";
-                            $result = mysqli_query($conn,$sql);
-                            if (mysqli_num_rows($result) != 0){
-                                while($row = mysqli_fetch_array($result)){
-                            
-                        ?>
-                        <tr>
-                            <td><?php echo $row[0];?></td>
-                            <td><?php echo $row[1];?></td>
-                            <td><?php if ($row[2] == 1){
-                                echo "Student";
-                            }
-                            else{
-                                echo "Not A Student";
-                            }
-                            ?></td>
-                            <td><?php if ($row[3] == 1){
-                                echo "Currently Renting";
-                            }
-                            else{
-                                echo "Not Currently Renting";
-                            }
-                            ?></td>
-                            <td><?php echo $row[4];?></td>
-                        </tr>
-                        <?php }
-                            }
+                        <?php
+                        require_once('DBconnect.php');
+                        $sql = "select * from renters";
+                        $result = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($result) != 0) {
+                            while ($row = mysqli_fetch_array($result)) {
+
+                                ?>
+                                <tr>
+                                    <td><?php
+                                    $sql1 = "select * from renters r join user_info u on u.nid = r.nid where u.nid = $row[0]";
+                                    $result1 = mysqli_query($conn, $sql1);
+                                    $row1 = mysqli_fetch_array($result1);
+                                    echo $row1[6]; ?></td>
+                                    <td><?php echo $row[1]; ?></td>
+                                    <td><?php if ($row[2] == 1) {
+                                        echo "Student";
+                                    } else {
+                                        echo "Not A Student";
+                                    }
+                                    ?></td>
+                                    <td><?php if ($row[3] == 1) {
+                                        echo "Currently Renting";
+                                    } else {
+                                        echo "Not Currently Renting";
+                                    }
+                                    ?></td>
+                                    <td><?php echo $row[4]; ?></td>
+                                </tr>
+                            <?php }
+                        }
                         ?>
                     </thead>
                 </table>
@@ -57,4 +60,5 @@
     </div>
     <script src="script.js"></script>
 </body>
+
 </html>
--- property.php
+++ property.php
@@ -1,18 +1,19 @@
 <?php
 require_once('DBconnect.php');
 require_once('session.php');
-if(isset($_POST['pin']) && isset($_POST['price']) && isset($_POST['flat_type'])){
+if (isset($_POST['pin']) && isset($_POST['price']) && isset($_POST['flat_type'])) {
     $p = $_POST['pin'];
     $ut = $_POST['util'];
-    $f = $_POST['is_furnished'] ? 1 : 0 ;
+    $f = $_POST['is_furnished'] ? 1 : 0;
     $pr = $_POST['price'];
     $t = $_POST['flat_type'];
     $sql = "insert into flat_plot_rent_selling values('$p','$ut','$f','$pr','$t')";
-    $result = mysqli_query($conn,$sql);
+    $result = mysqli_query($conn, $sql);
+    $sql = "insert into owns values('" . $_SESSION['user_id'] . "','$p')";
+    $result = mysqli_query($conn, $sql);
     header("Location: Home-page.php");
-    
-}
-else{
+
+} else {
     die("Have to select pin,price,flat_type");
 }
 ?>

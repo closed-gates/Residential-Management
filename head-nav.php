2b212e22205dbfe6750bc21ce1d4fe0d794efa18
closed-gates <ibsanhosainansary@gmail.com>
2026-01-20 19:19:30 +0600

    changing head-nav to have better nav

--- head-nav.php
+++ head-nav.php
@@ -1,12 +1,26 @@
 <nav class="sidebar">
     <div class="logo"><a href="Home-page.php">ResiCare</a></div>
     <ul>
-        <li class="active"><a href="Home-page.php">Dashboard</a></li>
-        <li class="card1"><a href="Residents-page.php">Residents</a></li>
-        <li class="card1"><a href="Service-page.php">Maintenance</a></li>
-        <li class="card1"><a href="payment-history.php">Payments</a></li>
-        <li class="card1"><a href="review-page.php">Review</a></li>
-        <li class="card1"><a href="grocery-page.php">Grocery</a></li>
-        <li class="card1"><a href="account-page.php">Account</a></li>
+        <a href="Home-page.php">
+            <li class="active">Dashboard
+        </a></li>
+        <a href="Residents-page.php">
+            <li class="card">Residents</li>
+        </a>
+        <a href="Service-page.php">
+            <li class="card">Maintenance</li>
+        </a>
+        <a href="payment-history.php">
+            <li class="card">Payments</li>
+        </a>
+        <a href="review-page.php">
+            <li class="card">Review</li>
+        </a>
+        <a href="grocery-page.php">
+            <li class="card">Grocery</li>
+        </a>
+        <a href="account-page.php">
+            <li class="card">Account</li>
+        </a>
     </ul>
 </nav>
